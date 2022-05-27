5f7796a06a2785c54fead2eaf64d41084a1ef219
tachanka61 <Tachanka61@protonmail.com>
2022-05-27 20:55:20 +0400

    Add dmenu-projects.sh, add DMENU_PROJECT_DIR to .dmenurc
    dmenu-projects.sh will list all folder and files in your project directory
    You can quickly open your project in any text editor

---
+++ scripts/dmenu-projects.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+#
+# ████████╗ |              DMENU-SCRIPTS
+# ╚══██╔══╝ |               tachanka61
+#    ██║    |
+#    ██║    | https://github.com/tachanka61/dmenu-scripts
+#    ██║    | Dependencies: dmenu, emacs (default editor)
+#    ╚═╝    |
+
+# shellcheck disable=SC1091
+
+# This makes program more secure
+# Also you can enable 'x' flag to enable debug
+set -euo pipefail
+
+# Load config
+if [[ -f "$HOME"/.dmenurc ]]; then
+  source "$HOME/.dmenurc"
+else
+  DMENU() {
+    input=$(echo -e "$1" | dmenu -i -l 18)
+  }
+  DMENU_EDITOR="xterm -e vim"
+  DMENU_PROJECT_DIR="$HOME/projects/"
+fi
+
+# Get project list
+content=$(ls --sort=time "$DMENU_PROJECT_DIR")
+
+DMENU "$content" "Select your project: "
+
+if [[ $input ]]; then
+  $DMENU_EDITOR "$input"
+fi
